75ec318db3006833017573fc3942c49ce9e8fc90
Antoine <mahassadijoseph@gmail.com>
2025-01-09 16:35:59 +0100

    2nd input-pswd components and documented

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import InputPassword from '../components/InputPassword/InputPassword'
 import InputEmail from '../components/InputEmail/InputEmail'
+import './SignUp.scss'
+
 
 function SignUp() {
     return (
         <>
-            <InputEmail />
+            <div className='wrapper-form'>
+                <div className='form-content'>
+                    <InputEmail />
+                    <InputPassword />
+                    < a href='/Reset.jsx' >
+                        <span className='fg-pwd'>Forgot Password ?</span>
+                    </a>
+                </div>
+
+                <div className='wrapper-side-cover'>
+
+                </div>
+
+
+            </div>
+
         </>
 
     )
